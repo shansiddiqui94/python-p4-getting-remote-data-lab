5f960dc9e98c1c70378933bda1b5a57c076376fe
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-04-09 14:34:12 -0400

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,17 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
+    def __init__(self, url):  #Setting the variables for the API linl 
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self):           # In this method I am getting back the raw JSON data
+        response = requests.get(self.url)
+        return response.content             #returning the raw JSON data
+
 
     def load_json(self):
-        pass
+        response_content = self.get_response_body() # calling my previous method to get the raw data 
+        json_data = json.loads(response_content) #converting the raw data into a python list or dict
+        return json_data #returning the new Python converted data
+
+        
